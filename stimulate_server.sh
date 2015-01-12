921ee5c2b47319da487ea16c1b279f8613a06e43
Cristián Rojas <injcristianrojas@gmail.com>
2015-01-12 19:17:04 -0300

    Improved server stimulation script

--- stimulate_server.sh
+++ stimulate_server.sh
@@ -1,9 +1,26 @@
 #!/usr/bin/env bash
 
+if [ -z "$1" ]
+  then
+    echo "Usage: stimulate_server.sh ip_address [time_between_requests]"
+    exit 1
+fi
+
+if [ -z "$2" ]
+  then
+    TIME_BETWEEN_REQS="1"
+  else
+    TIME_BETWEEN_REQS="$2"
+fi
+
+IP_ADDRESS="$1"
+
 while :
 do
-	RAND_STRING=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 32 | head -n 1)	
-	curl -v --insecure https://172.17.0.3?data=$RAND_STRING
-	sleep 1
+  USER=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 8 | head -n 1)
+  PASSWORD=$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 16 | head -n 1) 
+  curl -v --insecure -d "user=$USER&password=$PASSWORD" \
+    -A "User-Agent: Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.0;)" \
+    https://$IP_ADDRESS
+  sleep $TIME_BETWEEN_REQS
 done
-
